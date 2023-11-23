ee0162bacb691c98d561a6777a1d08a33b811423
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2023-11-23 15:40:34 +0100

    GET, POST, PUT, DELETE ok

--- src/main/java/webapp/demo/controller/HeroController.java
+++ src/main/java/webapp/demo/controller/HeroController.java
@@ -1,8 +1,6 @@
 package webapp.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import webapp.demo.model.Hero;
 
 import java.util.ArrayList;
@@ -12,7 +10,6 @@ import java.util.Optional;
 /**
  * Controller de Hero. Va s'occuper de tout ce qui est lié au Hero. Pour le moment intègre les méthodes getters et display
  * des Hero. Possède en attribut une List de Hero en ArrayList.
- *
  */
 @RestController
 public class HeroController {
@@ -24,8 +21,10 @@ public class HeroController {
     /**
      * Permet de récupérer la liste des héros sous forme de List de Hero. Pour le moment, les Hero sont ajoutés à la main dans
      * l'ArrayList heroesList présente en attribut de la classe.
+     *
      * @return heroesList, List de Hero
      */
+
     public List<Hero> getHeroesList() {
         heroesList.add(new Hero(1, "JB", "warrior", 5));
         heroesList.add(new Hero(2, "Nathalie", "wizard", 8));
@@ -35,6 +34,7 @@ public class HeroController {
 
     /**
      * Méthode permettant d'afficher tous les héros en format JSON dans le navigateur grâce à la mention @GetMapping.
+     *
      * @return une Liste de Hero
      */
     @GetMapping("/heroes")
@@ -46,6 +46,7 @@ public class HeroController {
      * Permet de récupérer les héros via leur id. Le int "id" mis en paramètre avec la mention @PathVariable est le même que l'id
      * présent dans l'extension de l'URL "/heroes/{id}". Cette méthode utilise un filter et retourne un Hero. La mention Optionnal est importante à spécifier
      * car, potentiellement, l'id demandé ne correspond à rien.
+     *
      * @param id - id du héros recherché
      * @return un Hero
      */
@@ -55,5 +56,20 @@ public class HeroController {
                 .stream()
                 .filter(hero -> hero.getId() == id)
                 .findFirst();
+    }
+
+    @PostMapping("/heroes")
+    public void addHero(@RequestBody Hero hero) {
+        heroesList.add(hero);
+    }
+
+    @PutMapping("/heroes/{id}")
+    public void updateHero(@RequestBody Hero hero, @PathVariable int id) {
+        heroesList.set(id, hero);
+    }
+
+    @DeleteMapping("/heroes/{id}")
+    public void deleteHero(@PathVariable int id) {
+        heroesList.remove(id);
     }
 }
