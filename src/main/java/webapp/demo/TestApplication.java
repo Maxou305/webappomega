ff4db38d31653961bb20674e65798fe249be3cb1
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2023-11-30 16:16:54 +0100

    connection to eureka ok

--- src/main/java/webapp/demo/TestApplication.java
+++ src/main/java/webapp/demo/TestApplication.java
@@ -2,11 +2,14 @@ package webapp.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Classe permettant le lancement de l'application grâce à la mention @SpringBootApplication.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class TestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/webapp/demo/controller/HeroController.java
+++ src/main/java/webapp/demo/controller/HeroController.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-//@Repository
 public class HeroController {
     private DAO heroDAO;
 
